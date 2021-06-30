3e020cf2d75738175acb6c2d97f094f6bfbf7588
glhcl <57966922+glhcl@users.noreply.github.com>
2021-06-30 11:20:34 -0400

    Update PHP_PathTraversalTest.php

--- PHP_PathTraversalTest.php
+++ PHP_PathTraversalTest.php
@@ -1,4 +1,8 @@
+<?php
+sha = new SHA1CryptoServiceProvider(); //Example 2 md5 = HashAlgorithm.Create("MD5");
 $file = $_GET['file'];
 $content = $_GET['content'];
 file_put_contents("/some/path/$file",$content);
 fclose($fh);
+?>
+
