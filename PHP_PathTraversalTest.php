51cf7c18a53a46ea3edff58f8a48617427328828
glhcl <57966922+glhcl@users.noreply.github.com>
2021-06-30 11:52:20 -0400

    Update PHP_PathTraversalTest.php

--- PHP_PathTraversalTest.php
+++ PHP_PathTraversalTest.php
@@ -1,4 +1,7 @@
+<?php
 $file = $_GET['file'];
 $content = $_GET['content'];
 file_put_contents("/some/path/$file",$content);
 fclose($fh);
+$mysqli = new mysqli("localhost", "scott", "tiger", "test");
+?>
